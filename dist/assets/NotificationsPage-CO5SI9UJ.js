5d28842fb7f531bbe550e4a88b329dd30cd643a8
Fernando Álisson <fernando.alisson@ufpe.br>
2026-06-29 18:04:35 -0300

    feat

--- dist/assets/NotificationsPage-CO5SI9UJ.js
+++ dist/assets/NotificationsPage-CO5SI9UJ.js
@@ -1,4 +1,4 @@
-import{c as w,u as ee,r,m as O,j as a,S as ae,B as te,b as se,f as ne}from"./index-DbB9E-o4.js";import{fetchNotifications as re,fetchCampaigns as ie,enableAdminPush as le,readNotification as $,createCampaign as oe}from"./notificationsService-CSonOUpC.js";/**
+import{c as w,u as ee,r,m as O,j as a,S as ae,B as te,b as se,f as ne}from"./index-neqxa24J.js";import{fetchNotifications as re,fetchCampaigns as ie,enableAdminPush as le,readNotification as $,createCampaign as oe}from"./notificationsService-D8g4T8Kg.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
